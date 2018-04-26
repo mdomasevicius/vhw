29ec97806442b6224beac0344be78647ec4121f7
Mantas Domaševičius <mdo@platform-lunar.com>
2018-04-26 15:50:46 +0300

    Make Discounts integration test useful

--- src/itest/groovy/lt/vinted/homework/discount/DiscountIntegrationSpec.groovy
+++ src/itest/groovy/lt/vinted/homework/discount/DiscountIntegrationSpec.groovy
@@ -1,49 +1,26 @@
 package lt.vinted.homework.discount
 
-import spock.lang.Shared
 import spock.lang.Specification
-import spock.lang.Unroll
 
 import java.time.LocalDate
 
 class DiscountIntegrationSpec extends Specification {
 
-    @Shared
-    def discounts = new Discounts()
+    def 'monthly discounts should not exceed total of 10.00 monetary units'() {
+        given:
+            def discounts = new Discounts()
+        expect:
+            (1..100).inject(0) { acc, ignored ->
+                acc += discounts.calculateDiscount(newOrder('2015-04-04', 'LP', 'L')).discount
+                acc
+            } <= 10.00
+    }
 
-    @Unroll
-    def 'Order(#date, #size, #provider) should cost #discountedPrice to deliver with total discount of #discount'() {
-        when:
-            def result = discounts.calculateDiscount(new Order(
-                date: LocalDate.parse(date),
-                size: size,
-                providerName: provider))
-        then:
-            result.discountedPrice == discountedPrice
-            result.discount == discount
-        where:
-            date         | size | provider || discountedPrice | discount
-            '2015-02-01' | 'S'  | 'MR'     || 1.50            | 0.50
-            '2015-02-02' | 'S'  | 'MR'     || 1.50            | 0.50
-            '2015-02-03' | 'L'  | 'LP'     || 6.90            | 0.00
-            '2015-02-05' | 'S'  | 'LP'     || 1.50            | 0.00
-            '2015-02-06' | 'S'  | 'MR'     || 1.50            | 0.50
-            '2015-02-06' | 'L'  | 'LP'     || 6.90            | 0.00
-            '2015-02-07' | 'L'  | 'MR'     || 4.00            | 0.00
-            '2015-02-08' | 'M'  | 'MR'     || 3.00            | 0.00
-            '2015-02-09' | 'L'  | 'LP'     || 0.00            | 6.90
-            '2015-02-10' | 'L'  | 'LP'     || 6.90            | 0.00
-            '2015-02-10' | 'S'  | 'MR'     || 1.50            | 0.50
-            '2015-02-10' | 'S'  | 'MR'     || 1.50            | 0.50
-            '2015-02-11' | 'L'  | 'LP'     || 6.90            | 0.00
-            '2015-02-12' | 'M'  | 'MR'     || 3.00            | 0.00
-            '2015-02-13' | 'M'  | 'LP'     || 4.90            | 0.00
-            '2015-02-15' | 'S'  | 'MR'     || 1.50            | 0.50
-            '2015-02-17' | 'L'  | 'LP'     || 6.80            | 0.10
-            '2015-02-17' | 'S'  | 'MR'     || 1.50            | 0.50
-            '2015-02-24' | 'L'  | 'LP'     || 6.90            | 0.00
-            '2015-02-24' | 'L'  | 'LP'     || 6.90            | 0.00
-            '2015-03-01' | 'S'  | 'MR'     || 1.50            | 0.50
+    private static Order newOrder(String date, String provider, String size) {
+        new Order(
+            date: LocalDate.parse(date),
+            provider: provider,
+            size: size)
     }
 
 }
